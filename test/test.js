d49c2e05e5cf2e02ca09465bb4d633e266c30c6a
Jaid <jaid.jsx@gmail.com>
2019-06-03 22:33:17 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,54 +1,15 @@
 import path from "path"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
-const {default: fetchYoutubeUploads,
-  sync: fetchYoutubeUploadsSync} = indexModule
+const {default: fetchYoutubeUploads, fetchUploadsForUser} = indexModule
 
-it("should get normalized", () => {
-  const result = fetchYoutubeUploadsSync({
-    name: "test ",
-    version: " 1.2.3",
-  })
-  expect(result).toMatchObject({
-    pkg: {
-      name: "test",
-      version: "1.2.3",
-    },
-    path: false,
-  })
-})
-
-it("should run async", async () => {
-  const result = await fetchYoutubeUploads({
-    name: "test",
-  })
-  expect(result).toMatchObject({
-    pkg: {
-      name: "test",
-    },
-    path: false,
-  })
-})
-
-it("should find the package.json of this module", () => {
-  const result = fetchYoutubeUploadsSync(path.resolve(__dirname, ".."))
-  expect(result).toMatchObject({
-    pkg: {
-      author: {
-        name: "Jaid",
-        email: "jaid.jsx@gmail.com",
-        url: "https://github.com/Jaid",
-      },
-    },
-  })
-})
-
-it("should find a json5 package", () => {
-  const result = fetchYoutubeUploadsSync(__dirname)
-  expect(result).toMatchObject({
-    path: expect.stringMatching(/package\.json5$/),
-    pkg: {
-      name: "is-json5",
-    },
-  })
+it("should run", async () => {
+  const [channelResult, userResult] = await Promise.all([
+    fetchYoutubeUploads("UCdC0An4ZPNr_YiFiYoVbwaw"), // Daily Dose of Internet
+    fetchUploadsForUser("HerrNewstime"),
+  ])
+  expect(channelResult.length).toBeGreaterThan(5)
+  expect(userResult.length).toBeGreaterThan(5)
+  expect(channelResult[0].id.length).toBeGreaterThan(5)
+  expect(channelResult[0].title.length).toBeGreaterThan(0)
 })
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,5 @@
 import {configureNodeLib} from "webpack-config-jaid"
 
 export default configureNodeLib({
-  documentation: {babel: true},
   publishimo: {fetchGithub: true},
 })
