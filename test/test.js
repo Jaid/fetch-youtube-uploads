1776e9f708f47c52003ca294c42d689145870369
Jaid <jaid.jsx@gmail.com>
2019-12-14 10:21:42 +0100

    Added new test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -22,3 +22,21 @@ it("should run with retries", async () => {
   expect(result[0].id.length).toBeGreaterThan(5)
   expect(result[0].title.length).toBeGreaterThan(0)
 })
+
+// I do not want to be unnecessarily edgy here, but I need a dead person's channel to ensure that no new uploads break the test case
+// I really loved your videos, Franc Tausch! ❤
+
+it("should return correct titles", async () => {
+  const result = await fetchUploadsForUser("FilmKritikTV", {
+    retries: 3,
+  })
+  expect(result.length).toBeGreaterThan(5)
+  expect(result[0]).toStrictEqual({
+    id: "kOOV1rLj6dc",
+    title: "Exklusiv: END OF WATCH Trailer german deutsch [HD]",
+  })
+  expect(result[1]).toStrictEqual({
+    id: "5R1zxR5Lnww",
+    title: "PARANORMAL ACTIVITY 4 Trailer german deutsch [HD]",
+  })
+})
